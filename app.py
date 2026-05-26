e3e1fb4c14021d0ad8e729cd2081af94f53b9a44
dmlau <dmlau@users.noreply.local>
2026-05-26 14:58:23 -0500

    Remove LWS completely & get VSC to support breakpoints in both python & js with a launch.json file

--- app.py
+++ app.py
@@ -270,7 +270,7 @@ def export_csv() -> Any:
 
 if __name__ == "__main__":
     #app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
 
 
